8a31e13ccaf43edb54fc66ba71889c2b6882f3c8
Santhosh Sivakulanathan <santhoshsivakulanathan@gmail.com>
2023-04-05 10:07:12 -0700

    adding some html stuff

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-function eval(num1, operator, num2){
+let num1, num2 = '0';
+let operator = '';
+const calculate = document.getElementById('equal');
+calculate.addEventListener('click', function(){
+  if(operator == ''){
+    return;
+  }else
+    evaluate(num1, operator, num1);
+})
+
+function setValues(e){
+  value = e.target.value;
+  if(value == '+' || value == '-' || value == 'x' || value == '/'){
+    operator = value;
+  }else if(operator == ''){
+    if(num2 == '0'){
+      num2 = value
+    }else{
+      num2 += value;
+    }
+  }else{
+    if(num1 == '0'){
+      num1 = value
+    }else{
+      num1 += value;
+    }
+  }
+}
+
+function evaluate(num1, operator, num2){
+  num1 = Number(num1);
+  num2 = Number(num2);
   switch(operator){
     case '+':
       return num1 + num2;
@@ -7,6 +38,17 @@ function eval(num1, operator, num2){
     case 'x':
       return num1*num2;
     case '/':
-      return num1/num2;
+      if(num2 == 0){
+        alert("Can't divide by zero");
+        reset();
+      }else
+        return num1/num2;
   }
 }
+
+
+
+console.log(evaluate('5', '+', 3))
+console.log(evaluate(5, '-', 3))
+console.log(evaluate(5, 'x', 3))
+console.log(evaluate(5, '/', 3))
