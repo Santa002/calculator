1bb0cd8d5952c5147c447a2f228dc3d63e87357e
Santhosh Sivakulanathan <santhoshsivakulanathan@gmail.com>
2023-04-05 05:50:15 -0700

    added couples lines

---
+++ script.js
@@ -0,0 +1,12 @@
+function eval(num1, operator, num2){
+  switch(operator){
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case 'x':
+      return num1*num2;
+    case '/':
+      return num1/num2;
+  }
+}
